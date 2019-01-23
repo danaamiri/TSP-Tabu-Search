7416e6205ebfc7f9b6a6951c37377ae7c3d0bb15
daarvag95 <pouria.mirelmi@gmail.com>
2019-01-23 15:52:13 +0330

    getSuccessor updated

--- getSuccessors.m
+++ getSuccessors.m
@@ -1,7 +1,18 @@
 function [neighbors , swaplist , evals] = getSuccessors (matrix,n)
   
-  combs = combnk (1 : size(matrix,1) , 2) ;
-  swaplist = combs(1 : n, :) ;
+  combs = combnk (2 : size(matrix,1)-1 , 2) ;
+  
+  a = randperm (size(combs,1));
+  b = a (1:n);
+  
+  for i = 1 : n
+    
+    swaplist (i,1) = combs(b(i),1);
+    swaplist (i,2) = combs(b(i),2);
+  
+  end
+  
+%  swaplist = combs(1:n, :) ;
   neighbors = [];
   
   for i = 1:size(swaplist,1)
@@ -22,7 +33,7 @@ function [neighbors , swaplist , evals] = getSuccessors (matrix,n)
   
   end
   
-  evals
   swaplist
+  evals
   
 end
