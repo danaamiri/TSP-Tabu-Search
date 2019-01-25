4a26db16ef5428ace06db279ac7c61ca1f51d2fe
danaamiri <dana.amiri@yahoo.com>
2019-01-25 15:19:06 +0330

    Buges fixed

--- Graphics/plotSolution.m
+++ Graphics/plotSolution.m
@@ -1,6 +1,8 @@
 function x =  plotSolution(best,current)
     x = [];
     y = [];
+    xs = [];
+    ys = [];
     g = graphGenerator(best);
     gs = graphGenerator(current);
     sz = size(best);
@@ -15,9 +17,19 @@ function x =  plotSolution(best,current)
        temp = best(i,:);
        y = [y,temp(3)];
     end
+    
+    for i = 1:n
+       temp = current(i,:);
+       xs = [xs,temp(2)];
+    end
+    
+    for i = 1:n
+       temp = current(i,:);
+       ys = [ys,temp(3)];
+    end
     p= plot(g,'Xdata',-y,'Ydata',x,'LineWidth',3);
     p.MarkerSize = 5;
     hold on;
-    plot(gs,'Xdata',-y,'Ydata',x,'LineWidth',1);
+    plot(gs,'Xdata',-ys,'Ydata',xs,'LineWidth',1);
     hold off;
 end
